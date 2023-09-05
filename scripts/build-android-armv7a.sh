754291d6a97c3c7f8d28b72953183b297a920622
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-09-05 21:14:06 +0300

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -2,8 +2,8 @@
 
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r10e-linux-x86_64.zip
+unzip -qq android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-./waf configure -T debug --android=armeabi-v7a-hard,4.9,21 --togles --disable-warns &&
+./waf configure -T debug --android=armeabi-v7a-hard,4.9,21 --build-game=tf_mod --togles --disable-warns &&
 ./waf build
